c85b2756dfe3e424ba5d21e05ff054d514d1122f
Florian Schäfer <florian.joh.schaefer@gmail.com>
2021-05-13 22:22:26 +0200

    Fix secret name

--- tonietools/tonies_helper.py
+++ tonietools/tonies_helper.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import List, Optional
 
 from tonietools.tonie_api import TonieAPI
 
@@ -9,6 +9,19 @@ def get_tonies_api():
     api.households_update()
     api.households[os.environ["TONIE_HOUSEHOLD"]].creativetonies_update()
     return api
+
+
+def list_households() -> List[str]:
+    api = TonieAPI(os.environ["TONIE_MAIL"], os.environ["TONIE_PASSWORD"])
+    api.households_update()
+    return list(api.households.keys())
+
+
+def list_tonies(household_id: Optional[str] = None) -> List[str]:
+    api = TonieAPI(os.environ["TONIE_MAIL"], os.environ["TONIE_PASSWORD"])
+    api.households_update()
+    api.households[os.environ["TONIE_HOUSEHOLD"]].creativetonies_update()
+    return api.households[household_id or os.environ["TONIE_HOUSEHOLD"]]
 
 
 def upload(
