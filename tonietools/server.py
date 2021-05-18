dbdf356b8be46cf2db83f5eb052f6eebc2c59bee
Florian Schäfer <33159547+floscha@users.noreply.github.com>
2021-05-18 21:13:56 +0200

    Add Web GUI MVP (resolves #1)
    * Add web frontend for MVP
    
    * Add backend for MVP
    
    * Make running web server the default Docker command
    
    * Adjust README
    
    * Fix formatting issue
    
    * Remove unused import

---
+++ tonietools/server.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+from tonietools.youtube_importer import import_youtube_video
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    import os
+
+    cwd = os.getcwd()
+    print(cwd)
+    return HTMLResponse(open("static/index.html", "r").read())
+
+
+class Video(BaseModel):
+    url: str
+    name: Optional[str]
+    start: Optional[str]
+    end: Optional[str]
+
+
+@app.post("/youtube")
+async def youtube(videos: List[Video]):
+    for v in videos:
+        import_youtube_video(v.url, v.name, v.start, v.end)
+    return f"Uploaded {len(videos)} items"
+
+
+def run_server(host="0.0.0.0", port=8000):
+    uvicorn.run(app, host=host, port=port)
+
+
+if __name__ == "__main__":
+    run_server()
