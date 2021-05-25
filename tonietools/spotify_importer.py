b47be7a96097005f8cfdcd411656fdc3f53c5bcc
Florian Schäfer <florian.joh.schaefer@gmail.com>
2021-05-25 20:43:09 +0200

    Format with black

--- tonietools/spotify_importer.py
+++ tonietools/spotify_importer.py
@@ -14,28 +14,29 @@ from tonietools.tonies_helper import get_tonies_api, upload
 def download(request: str):
     # From https://github.com/spotDL/spotify-downloader/blob/master/spotdl/__main__.py
     SpotifyClient.init(
-        client_id='5f573c9620494bae87890c0f08a60293',
-        client_secret='212476d9b0f3472eaa762d90b19b0ba8',
-        user_auth={}
+        client_id="5f573c9620494bae87890c0f08a60293",
+        client_secret="212476d9b0f3472eaa762d90b19b0ba8",
+        user_auth={},
     )
 
     with DownloadManager() as downloader:
-        if 'open.spotify.com' in request and 'track' in request:
+        if "open.spotify.com" in request and "track" in request:
             song = SongObj.from_url(request)
 
             if song is not None:
                 if song.get_youtube_link() is not None:
                     downloader.download_single_song(song)
                 else:
-                    print('Skipping %s (%s) as no match could be found on youtube' % (
-                        song.get_song_name(), request
-                    ))
+                    print(
+                        "Skipping %s (%s) as no match could be found on youtube"
+                        % (song.get_song_name(), request)
+                    )
 
-        elif 'open.spotify.com' in request and 'album' in request:
+        elif "open.spotify.com" in request and "album" in request:
             songObjList = get_album_tracks(request)
             downloader.download_multiple_songs(songObjList)
 
-        elif 'open.spotify.com' in request and 'playlist' in request:
+        elif "open.spotify.com" in request and "playlist" in request:
             songObjList = get_playlist_tracks(request)
             downloader.download_multiple_songs(songObjList)
 
