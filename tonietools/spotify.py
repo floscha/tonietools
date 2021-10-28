3c088b7b5d2117f26b419b20e736445f7f81e1e6
Florian Schäfer <florian.joh.schaefer@gmail.com>
2021-10-28 14:50:51 +0200

    Fix and refactor Spotify functionality

--- tonietools/spotify.py
+++ tonietools/spotify.py
@@ -1,19 +1,9 @@
 import os
-from pathlib import Path
 from typing import List
 
 from spotdl.download.downloader import DownloadManager
+from spotdl.search import song_gatherer
 from spotdl.search.spotify_client import SpotifyClient
-
-from tonietools.tonies_helper import get_tonies_api, upload
-
-# from spotdl.search.song_obj import SongObj
-# from spotdl.search.spotifyClient import SpotifyClient
-# from spotdl.search.utils import (
-#     get_playlist_tracks,
-#     get_album_tracks,
-# )
-
 
 DEFAULT_SPOTIFY_ID = "5f573c9620494bae87890c0f08a60293"
 DEFAULT_SPOTIFY_SECRET = "212476d9b0f3472eaa762d90b19b0ba8"
@@ -46,32 +36,13 @@ class Spotify:
     def download(self, request: str):
         with DownloadManager() as downloader:
             if "open.spotify.com" in request and "track" in request:
-                song = SongObj.from_url(request)
-
-                if song is not None:
-                    if song.get_youtube_link() is not None:
-                        downloader.download_single_song(song)
-                    else:
-                        print(
-                            "Skipping %s (%s) as no match could be found on youtube"
-                            % (song.get_song_name(), request)
-                        )
+                song = song_gatherer.from_spotify_url(request)
+                downloader.download_single_song(song)
 
             elif "open.spotify.com" in request and "album" in request:
-                songObjList = get_album_tracks(request)
+                songObjList = song_gatherer.from_album(request)
                 downloader.download_multiple_songs(songObjList)
 
             elif "open.spotify.com" in request and "playlist" in request:
-                songObjList = get_playlist_tracks(request)
+                songObjList = song_gatherer.from_playlist(request)
                 downloader.download_multiple_songs(songObjList)
-
-
-# def sync(request: str):
-#     download(request)
-
-#     downloaded_files = Path().glob("*.mp3")
-#     api = get_tonies_api()
-
-#     for f in downloaded_files:
-#         upload(api, str(f), str(f).split("- ")[1].split(".mp3")[0])
-#         f.unlink()
